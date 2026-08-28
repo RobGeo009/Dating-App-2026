6592e111e35655f5ac52b57af45c9b16fe195fa8
RobGeo009 <robgeoinfotech@gmail.com>
2026-08-28 16:41:49 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+builder.Services.AddCors();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
